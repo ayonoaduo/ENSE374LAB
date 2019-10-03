d670dcb5b8ac817399511805386ffbe6531970c7
Onisokien Ayonoadu <ayonoaduo@gmail.com>
2019-10-03 12:20:53 -0600

    List Element first commit

---
+++ ListElement.java
@@ -0,0 +1,29 @@
+public class ListElement {
+  private ListElement next;
+  //private ListElement previous;
+  private int data;
+
+  public ListElement ()
+  {
+    this.data = 0;
+    this.next = null;
+    //this.previous = null ;
+  }
+
+  public void setData(int data)
+  {
+    this.data=data;
+  }
+  public int getData()
+  {
+    return this.data;
+  }
+  public void setNext(ListElement next)
+  {
+    this.next=next;
+  }
+  public ListElement getNext()
+  {
+    return this.next;
+  }
+}
