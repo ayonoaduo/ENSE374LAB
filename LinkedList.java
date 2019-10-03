5dc3b8eb7e477f848c5cc4a37347c0d3ee6d6b09
Onisokien Ayonoadu <ayonoaduo@gmail.com>
2019-10-03 12:21:06 -0600

    Linked list first commit

---
+++ LinkedList.java
@@ -0,0 +1,34 @@
+public class LinkedList{
+
+  private ListElement head;
+
+  public LinkedList()
+  {
+    head == null;
+  }
+  public void addElement(ListElement le)
+  {
+    le.setNext(head);
+    if(head!=null)
+    {
+      head = le;
+    }
+
+  }
+  public ListElement getElement(int index)
+  {
+
+  }
+
+  public ListElement deleteElement(int index)
+  {
+
+  }
+
+  public void printLinkedListHead()
+  {
+
+  }
+
+
+}
