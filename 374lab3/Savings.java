0162613544005ed295ccbb37665ad6560ecf1d1a
Onisokien Ayonoadu <ayonoaduo@gmail.com>
2019-10-18 16:57:28 -0600

    Savings Commit

---
+++ 374lab3/Savings.java
@@ -0,0 +1,36 @@
+public class Savings{
+  private static double annualInterestRate;
+  private double savingsBalance;
+
+  public static void modifyAnnualInterestRate(double newRate){
+    annualInterestRate = newRate;
+  }
+
+  Savings(double balance){
+    savingsBalance=balance;
+  }
+
+  double getAnnInt(){
+    return annualInterestRate;
+  }
+
+  double getSavBal(){
+    return savingsBalance;
+  }
+
+  public double calcMonthlyInterest(){
+    double monthint= annualInterestRate / 12;
+    double monthIntBalance= savingsBalance * monthint;
+    savingsBalance= savingsBalance - monthIntBalance;
+    return savingsBalance;
+  }
+
+  public double calcBiWeeklyInterest(){
+		double interest= (1 + annualInterestRate/26);
+		double result = (Math.pow(interest, 26))-1;
+		double bal = savingsBalance*result;
+		savingsBalance = savingsBalance - bal;
+
+		return savingsBalance;
+	}
+}
