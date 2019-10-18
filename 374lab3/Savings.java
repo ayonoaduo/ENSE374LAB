45da998ca4e502b6a1b1a70a4c6d2d18d3bfaac2
Onisokien Ayonoadu <ayonoaduo@gmail.com>
2019-10-18 17:05:31 -0600

    Saving update

--- 374lab3/Savings.java
+++ 374lab3/Savings.java
@@ -2,33 +2,49 @@ public class Savings{
   private static double annualInterestRate;
   private double savingsBalance;
 
-  public static void modifyAnnualInterestRate(double newRate){
+  public static void modifyAnnualInterestRate(double newRate)
+  {
     annualInterestRate = newRate;
   }
 
-  Savings(double balance){
-    savingsBalance=balance;
+  Savings(double balance)
+  {
+    savingsBalance = balance;
   }
 
-  double getAnnInt(){
+  double getAnnInt()
+  {
     return annualInterestRate;
   }
 
-  double getSavBal(){
+  double getSavBal()
+  {
     return savingsBalance;
   }
 
-  public double calcMonthlyInterest(){
+  public double deposit(double deposit)
+  {
+    return (savingsBalance + deposit);
+  }
+
+  public double withdraw(deposit withdraw)
+  {
+    return (savingsBalance - withdraw);
+  }
+
+  public double calcMonthlyInterest()
+  {
     double monthint= annualInterestRate / 12;
     double monthIntBalance= savingsBalance * monthint;
     savingsBalance= savingsBalance - monthIntBalance;
     return savingsBalance;
   }
 
-  public double calcBiWeeklyInterest(){
+  public double calcBiWeeklyInterest()
+  {
 		double interest= (1 + annualInterestRate/26);
 		double result = (Math.pow(interest, 26))-1;
-		double bal = savingsBalance*result;
+		double bal = savingsBalance * result;
 		savingsBalance = savingsBalance - bal;
 
 		return savingsBalance;
